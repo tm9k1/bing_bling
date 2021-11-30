27697d19ac32f800424504ea93df3a161640997e
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2021-11-30 20:33:36 +0530

    remove dependency on lxml and urllib3
    we can just use 'html.parser' for parsing and use requests for downloading the webpage. Plus, it makes the code smaller and more reusable!

--- bing_bling.py
+++ bing_bling.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 import requests
 import  concurrent.futures
 from tqdm import tqdm
-import urllib3
 
 # change these two to whatever you like.
 # NOTE: Remember that the next run would again download all wallpapers if you move the wallpapers away from wallpaper_save_path
@@ -12,31 +11,41 @@ wallpaper_save_path = './Wallpapers/'
 webpages_save_path = './webpages/'
 
 # This is the link I use to get the wallpapers. This site is updated daily, with latest wallpapers at the top.
-url = 'https://www.bwallpaperhd.com/sitemap.html'
+sitemap_url = 'https://www.bwallpaperhd.com/sitemap.html'
 
-os.makedirs(wallpaper_save_path, exist_ok=True)
-os.makedirs(webpages_save_path, exist_ok=True)
+sitemap_filename = webpages_save_path + sitemap_url.split('/')[-1]
 
-def getWebPage(url: str, page_filename: str):
-    # Making the request (The request function returns HTTPResponse object)
+# download a file at download_url to file_name
+def download_a_file(download_url: str, file_name: str):
+    incomplete_file_name = file_name[:file_name.rfind('/')+1] + '0INCOMPLETE_'+ file_name[file_name.rfind('/')+1:]
 
-    http = urllib3.PoolManager()
+    # remove any stale download of the file
+    if os.path.exists(incomplete_file_name):
+        os.remove(incomplete_file_name)
 
-    resp = http.request('GET', url)
+    # download the file
+    response = requests.get(download_url, stream=True)
+    if response.status_code == 200:
 
-    if resp.status != 200:
-        print('We got a problem. The website is not working.. Error Code: ' + str(resp.status))
-        exit()
+        # write it as 0INCOMPLETE_filename so we can ensure removal of stale files later
+        # NOTE: This is not a silver bullet, but it's better than keeping our fingers crossed
+        with open(incomplete_file_name, 'wb') as f:
+            for chunk in response:
+                f.write(chunk)
 
-    # Download the page
-    if os.path.exists(webpages_save_path + page_filename):
-        os.remove(webpages_save_path + page_filename)
-    with open(webpages_save_path + page_filename, 'w', encoding="utf-8") as f:
-        f.write(resp.data.decode('utf-8'))
+    # rename the file to filename if we received the whole file
+    os.rename(incomplete_file_name, file_name)
 
+# returns a list of tuples as [(Wallpaper Name, http://wallpaper.webpage/url.html),...]
+def extract_links_from_sitemap(sitemap_url: str, sitemap_filename: str):
 
-def extract_links_from_sitemap(page_filename: str):
-    soup = BeautifulSoup(open(webpages_save_path + page_filename, encoding='utf-8'), features="lxml")
+    # remove any stale download of the sitemap
+    if os.path.exists(sitemap_filename):
+        os.remove(sitemap_filename)
+
+    # download the sitemap
+    download_a_file(sitemap_url, sitemap_filename)
+    soup = BeautifulSoup(open(sitemap_filename, encoding='utf-8'), "html.parser")
     # print(soup.prettify())
     divs = soup.find_all('div', attrs={'id':'content'})
     list_of_wallpprs = []
@@ -45,64 +54,58 @@ def extract_links_from_sitemap(page_filename: str):
         if h3:
             anchor_tags = div.find_all('a')
             for anchor_tag in anchor_tags:
-                # print(anchor_tag)
                 wallpaper_name = anchor_tag.contents[0]
                 wallpaper_webpage_url = anchor_tag.get('href')
                 list_of_wallpprs.append((wallpaper_name, wallpaper_webpage_url))
     return list_of_wallpprs
 
-
-
-# this function is instantiated across 40 threads
+# expects the tuples from list_of_wallpapers as [(Wallpaper Name, http://wallpaper.webpage/url.html),...]
 def download_a_wallpaper(tup: tuple):
 
     wallpaper_name, wallpaper_webpage_url = tup
     wallpaper_webpage_filename = wallpaper_webpage_url.split('/')[-1]
     wallpaper_filename = wallpaper_webpage_filename[:wallpaper_webpage_filename.find('.')]
-    wallpaper_filename = wallpaper_filename + '.jpg'
+    wallpaper_filename = wallpaper_filename + '.jpg' # hopefully they won't ditch jpg anytime soon!
 
     if not os.path.exists(wallpaper_save_path + wallpaper_filename):
-        getWebPage(wallpaper_webpage_url, wallpaper_webpage_filename)
-        soup = BeautifulSoup(open(webpages_save_path + wallpaper_webpage_filename, encoding='utf-8'), features="lxml")
-        # print(soup.prettify())
+        if not os.path.exists(webpages_save_path + wallpaper_webpage_filename):
+            download_a_file(wallpaper_webpage_url, webpages_save_path + wallpaper_webpage_filename)
+        soup = BeautifulSoup(open(webpages_save_path + wallpaper_webpage_filename, encoding='utf-8'), "html.parser")
         divs = soup.find_all('div', attrs={'class':'download'})
         for div in divs:
             anchors = div.find_all('a')
             for anchor in anchors:
                 if anchor.contents[0].strip().lower() == 'original':
                     wallpaper_url = anchor.get('href')
-                    # print(wallpaper_name + ': ' + wallpaper_url)
-                    if os.path.exists(wallpaper_save_path + '0INCOMPLETE_' + wallpaper_filename):
-                        os.remove(wallpaper_save_path + '0INCOMPLETE_' + wallpaper_filename)
-
-                    response = requests.get(wallpaper_url, stream=True)
-                    if response.status_code == 200:
-                        with open(wallpaper_save_path + '0INCOMPLETE_' + wallpaper_filename, 'wb') as f:
-                            for chunk in response:
-                                f.write(chunk)
-                    os.rename(wallpaper_save_path + '0INCOMPLETE_' + wallpaper_filename, wallpaper_save_path + wallpaper_filename)
+                    print(wallpaper_name + ': ' + wallpaper_url)
+                    download_a_file(wallpaper_url, wallpaper_save_path + wallpaper_filename)
         os.remove(webpages_save_path + wallpaper_webpage_filename)
-    return wallpaper_name
 
+    return wallpaper_name
 
 ## MAIN ACTION IS HERE
 
-page_filename = url.split('/')[-1]
+# make the necessary directories in case they don't exist
+os.makedirs(wallpaper_save_path, exist_ok=True)
+os.makedirs(webpages_save_path, exist_ok=True)
 
-getWebPage(url, page_filename)
-list_of_wallpapers = extract_links_from_sitemap(page_filename)
+# extract the list of wallpapers with links to each wallpaper's own webpage
+list_of_wallpapers = extract_links_from_sitemap(sitemap_url, sitemap_filename)
+# get the total count of wallpapers we can expect by the end of processing
 total_wallpapers = len(list_of_wallpapers)
-
+# Create a progress bar so user knows what's going on
 progress_bar = tqdm(range(total_wallpapers), desc="Downloading Wallpapers",colour="blue", bar_format='{l_bar}{bar}| [{n_fmt}/{total_fmt}] [Elapsed: {elapsed}]')
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=20, thread_name_prefix='Wallpaper_Downloader_') as executor:    
+# Start distributing work across multiple threads. 20 seems to be a safe count for now.
+with concurrent.futures.ThreadPoolExecutor(max_workers=20, thread_name_prefix='Wallpaper_Downloader_') as executor:
     future_to_url = [ executor.submit(download_a_wallpaper, wallpaper_entry) for wallpaper_entry in list_of_wallpapers ]
-    
+
     for future in concurrent.futures.as_completed(future_to_url):
         progress_bar.n += 1
         progress_bar.refresh()
+
 progress_bar.close()
-os.remove(webpages_save_path + page_filename)
+# clean up the downloaded sitemap webpage
+os.remove(sitemap_filename)
 
 print('Done! Enjoy your new collection! =)')
-
